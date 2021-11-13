0038fa35f25ad3c493bb75761e1a67d72063db9e
arunchristo <94177966+arunchristo@users.noreply.github.com>
2021-11-13 13:24:08 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,10 +12,10 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*❤️MAALUTTY💙 created by Nijin and husni*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*❤️MAALUTTY💙 created by Arun and christo*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n❤️MAALUTTY💙 created by Nijin and husni', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n❤️MAALUTTY💙 created by Arun and christo', MessageType.text);
         }
     }));
 }
@@ -24,16 +24,16 @@ else if (Config.WORKTYPE == 'public') {
 
     Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '919778797059-1634557305@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*❤️MAALUTTY💙 created by Nijin and husni*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*❤️MAALUTTY💙 created by Arun and christo*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n❤️MAALUTTY💙 created by Nijin and husni', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER + '\n❤️MAALUTTY💙 created by Arun and christo', MessageType.text);
         }
     }));
 }
